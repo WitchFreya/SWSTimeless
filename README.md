e42ba87de064f83820718037960a40976716306b
Freya Gaynor <freya@webwitch.org>
2023-07-21 11:40:30 -0500

    feature: Remaining shemaghs.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Scarlet Wolf Sorority Addons
+# Scarlet Wolf Sorority Timeless
 
 This project uses [HEMTT](https://brettmayson.github.io/HEMTT/) for build and configuration.
 
@@ -7,3 +7,4 @@ If you stumble upon this project, you are welcome to reuse the assets for your o
 ## Special Thanks
 
 - [Advanced Developer Tools](https://steamcommunity.com/sharedfiles/filedetails/?id=2369477168): My friend, you are a genius.
+- [OPCAN](https://steamcommunity.com/sharedfiles/filedetails/?id=2016312756)
---
+++ mod.cpp
@@ -0,0 +1,19 @@
+#define ICON "logo_ca.paa"
+#define DESCRIPTION "Addons for SWS items that are not dependent on being in OPTRE."
+
+// https://community.bistudio.com/wiki/Mod.cpp/bin_File_Format
+name = "SWS Timeless";
+author = "Maid";
+logo = ICON;
+logoOver = ICON;
+tooltip = "Scarlet Wolf Sorority Timeless";
+tooltipOwned = "SWS Timeless Owned";
+picture = ICON;
+actionName = "GitHub";
+action = "https://github.com/WitchFreya/SWSTimeless";
+overview = DESCRIPTION;
+overviewPicture = ICON;
+overviewText = DESCRIPTION;
+overviewFootnote = "<br/><br/><t color='#999999'>This content is under the APL-SA License.</t>";
+description = DESCRIPTION;
+dir = "@swst";
